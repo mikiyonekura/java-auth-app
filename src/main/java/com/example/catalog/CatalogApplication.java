07f0d61728e91b999cf424165f6977a9e7d10810
yonekuramiki <mikiyonekura4040@gmail.com>
2023-12-11 16:37:16 +0900

    Create motos contoller

--- src/main/java/com/example/catalog/CatalogApplication.java
+++ src/main/java/com/example/catalog/CatalogApplication.java
@@ -3,6 +3,7 @@ package com.example.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CatalogApplication {
 
