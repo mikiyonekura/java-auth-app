2c1288975d3f1e8743f590c889be0e3a5d120a83
yonekuramiki <mikiyonekura4040@gmail.com>
2024-01-03 21:58:40 +0900

    Themleafで情報を動的に表示

--- src/main/java/com/example/catalog/controllers/MotosController.java
+++ src/main/java/com/example/catalog/controllers/MotosController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
+import com.example.catalog.bean.User;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.time.LocalDateTime;
+
 @Controller
 public class MotosController {
 
@@ -14,9 +20,17 @@ public class MotosController {
         return "test";
     }
 
-    @RequestMapping("/motoList")
-    public String motoList() {
-        return "motoList";
+    @RequestMapping("/users")
+    public String motos(Model Model)  {
+        //ユーザー一覧を取得
+        List<User> users = new ArrayList<>();
+        users.add(new User(1,"米倉未樹","2000.8.5","mikiyonekura4040@gmail.com","12345", "admin", LocalDateTime.now(), LocalDateTime.now()));
+        users.add(new User(2,"橋本沙知","2001.3.20","hashimoto4040@gmail.com","12345", "enable", LocalDateTime.now(), LocalDateTime.now()));
+
+        //ユーザー一覧をModelに登録(Themeleafで参照できるようにする)
+        Model.addAttribute("users", users);
+
+        return "userList";
     }
 
 }
